8dca6f55dde710eefe1ff54815bdcc5487cec9d4
Yash Paneliya <yashsp1158@gmail.com>
2020-10-06 00:35:52 +0530

    Add files via upload

---
+++ XD-Samples/iPhoneXXS11Pro7.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'package:flutter_svg/flutter_svg.dart';
+
+class iPhoneXXS11Pro7 extends StatelessWidget {
+  iPhoneXXS11Pro7({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffed6337),
+      body: Stack(
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset(0.5, 335.5),
+            child: SvgPicture.string(
+              _shapeSVG_7118c9fd43be46918ee7b76a12685ee2,
+              allowDrawingOutsideViewBox: true,
+            ),
+          ),
+          Transform(
+            transform: Matrix4(
+                -0.309017,
+                0.951057,
+                0.0,
+                0.0,
+                -0.951057,
+                -0.309017,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                481.71,
+                315.88,
+                0.0,
+                1.0),
+            child: Container(
+              width: 78.0,
+              height: 617.0,
+              decoration: BoxDecoration(
+                color: const Color(0xffd46a4a),
+                boxShadow: [
+                  BoxShadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(-6, 6),
+                      blurRadius: 6)
+                ],
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(0.956305, 0.292372, 0.0, 0.0, -0.292372,
+                0.956305, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 348.3, 114.1, 0.0, 1.0),
+            child: Container(
+              width: 78.0,
+              height: 775.0,
+              decoration: BoxDecoration(
+                color: const Color(0xffd46a4a),
+                boxShadow: [
+                  BoxShadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(-6, 6),
+                      blurRadius: 6)
+                ],
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(
+                0.956305,
+                0.292372,
+                0.0,
+                0.0,
+                -0.292372,
+                0.956305,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                385.9,
+                252.08,
+                0.0,
+                1.0),
+            child: Container(
+              width: 78.0,
+              height: 617.0,
+              decoration: BoxDecoration(
+                color: const Color(0xffeb724b),
+                boxShadow: [
+                  BoxShadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(-6, 6),
+                      blurRadius: 6)
+                ],
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(
+                0.956305,
+                0.292372,
+                0.0,
+                0.0,
+                -0.292372,
+                0.956305,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                522.31,
+                64.78,
+                0.0,
+                1.0),
+            child: Container(
+              width: 78.0,
+              height: 905.0,
+              decoration: BoxDecoration(
+                color: const Color(0xffd46a4a),
+                boxShadow: [
+                  BoxShadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(-6, 6),
+                      blurRadius: 6)
+                ],
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(
+                -0.309017,
+                0.951057,
+                0.0,
+                0.0,
+                -0.951057,
+                -0.309017,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                505.2,
+                241.6,
+                0.0,
+                1.0),
+            child: Container(
+              width: 78.0,
+              height: 617.0,
+              decoration: BoxDecoration(
+                color: const Color(0xffeb724b),
+                boxShadow: [
+                  BoxShadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(-6, 6),
+                      blurRadius: 6)
+                ],
+              ),
+            ),
+          ),
+          Transform(
+            transform: Matrix4(
+                0.309017,
+                -0.951057,
+                0.0,
+                0.0,
+                0.951057,
+                0.309017,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                -137.1,
+                34.17,
+                0.0,
+                1.0),
+            child: Container(
+              width: 78.0,
+              height: 596.0,
+              decoration: BoxDecoration(
+                color: const Color(0xffd46a4a),
+                boxShadow: [
+                  BoxShadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(-6, 6),
+                      blurRadius: 6)
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const String _shapeSVG_7118c9fd43be46918ee7b76a12685ee2 =
+    '<svg viewBox="0.5 335.5 375.0 543.0" ><g transform=""><defs><filter id="shadow"><feDropShadow dx="0" dy="3" stdDeviation="6"/></filter></defs><path transform="translate(0.5, 335.5)" d="M 0 0 L 375 122" fill="none" fill-opacity="0.3" stroke="#ffffff" stroke-width="4" stroke-miterlimit="4" stroke-linecap="butt" filter="url(#shadow)"/><path transform="translate(0.5, 396.5)" d="M 0 0 L 375 122" fill="none" fill-opacity="0.3" stroke="#ffffff" stroke-width="4" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(0.5, 451.5)" d="M 0 0 L 375 122" fill="none" fill-opacity="0.3" stroke="#ffffff" stroke-width="4" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(0.5, 512.5)" d="M 0 0 L 375 122" fill="none" fill-opacity="0.3" stroke="#ffffff" stroke-width="4" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(0.5, 573.5)" d="M 0 0 L 375 122" fill="none" fill-opacity="0.3" stroke="#ffffff" stroke-width="4" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(0.5, 634.5)" d="M 0 0 L 375 122" fill="none" fill-opacity="0.3" stroke="#ffffff" stroke-width="4" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(0.5, 695.5)" d="M 0 0 L 375 122" fill="none" fill-opacity="0.3" stroke="#ffffff" stroke-width="4" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(0.5, 756.5)" d="M 0 0 L 375 122" fill="none" fill-opacity="0.3" stroke="#ffffff" stroke-width="4" stroke-miterlimit="4" stroke-linecap="butt" /></g></svg>';
